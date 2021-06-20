74ce82cf252c3bf08a17e1a98e626d00d3e5fcf8
Harshitha-11 <84697229+Harshitha-11@users.noreply.github.com>
2021-06-20 12:56:57 +0530

    Update Program-2.c

--- Program-2.c
+++ Program-2.c
@@ -1,17 +1,9 @@
-//PRINTING FIRST N NATURAL NUMEBRS
-//INPUT -3
-//OUTPUT - 1 2 3
-
-
 #include<stdio.h>
 int main()
 {
 	int i=1,n;
-//	printf("Enter upper limit : ");
 	scanf("%d",&n);
-	printf("The natural numbers are ");
 	for(i=1;i<=n;i++)
-	      	printf("%d",i);
-          return 0;
-	//Fill the missing code here
+		printf(" %d",i);
+   return 0;
  }
